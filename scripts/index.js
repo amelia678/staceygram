0a910a145159b313ab54d72826bf7b42f6295f60
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-10-12 13:53:29 -0400

    creates array of images

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+// Instaclone!
+
+const IMAGES = [
+
+"https://images.pexels.com/photos/1287365/pexels-photo-1287365.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+
+
+"https://images.pexels.com/photos/881142/pexels-photo-881142.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+
+
+"https://images.pexels.com/photos/1307503/pexels-photo-1307503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+
+
+];
